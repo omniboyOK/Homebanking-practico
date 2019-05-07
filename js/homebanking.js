cb5713d4892c41c6343488fe8b4224aa1991f315
Omniboy <kachorritou@gmail.com>
2019-05-07 20:02:14 -0300

    Ingresar usuario + extracción + ingreso

--- js/homebanking.js
+++ js/homebanking.js
@@ -1,24 +1,54 @@
 //Declaración de variables
+var nombreUsuario = "";
+var saldoCuenta = 4000;
+var limiteExtraccion = 1000;
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
+    iniciarSesion();
     cargarNombreEnPantalla();
     actualizarSaldoEnPantalla();
     actualizarLimiteEnPantalla();
 }
 
-
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
-
+    var nuevoLimite = prompt("Ingrese el nuevo limite de extracción");
+    nuevoLimite = parseInt(nuevoLimite);
+    limiteExtraccion = nuevoLimite;
+    actualizarLimiteEnPantalla();
 }
 
 function extraerDinero() {
-
+    var saldoAnterior = saldoCuenta;
+    var sumaDinero = prompt("Indique una cantidad a retirar");
+    sumaDinero = parseInt(sumaDinero);
+    if(sumaDinero == Number(sumaDinero) && (sumaDinero <= saldoCuenta)) {
+        if((sumaDinero <= limiteExtraccion)){
+            saldoCuenta -= sumaDinero;
+        actualizarSaldoEnPantalla();
+        alert("Usted extrajo: " + sumaDinero 
+              + "\nSu saldo anterior: " + saldoAnterior
+              + "\nSu saldo actual: " + saldoCuenta);
+        }
+    } else {
+        alert("La cuenta no posee fondos suficientes");
+    }
 }
 
 function depositarDinero() {
-
+    var saldoAnterior = saldoCuenta;
+    var sumaDinero = prompt("Indique una cantidad a ingresar");
+    sumaDinero = parseInt(sumaDinero);
+    if(sumaDinero == Number(sumaDinero)){
+        saldoCuenta += sumaDinero;
+        actualizarSaldoEnPantalla();
+        alert("Usted deposito: " + sumaDinero 
+              + "\nSu saldo anterior: " + saldoAnterior
+              + "\nSu saldo actual: " + saldoCuenta);
+    } else {
+        alert("El valor ingresado no es valido");
+    }
 }
 
 function pagarServicio() {
@@ -30,7 +60,7 @@ function transferirDinero() {
 }
 
 function iniciarSesion() {
-
+    nombreUsuario = prompt("Ingresa el nombre de tu usuario");
 }
 
 //Funciones que actualizan el valor de las variables en el HTML
